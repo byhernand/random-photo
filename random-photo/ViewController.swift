bf53140453f976b0c5a3d687589c47f0e7729c16
Eduardo Hernand <byhernand@hotmail.com>
2023-01-15 14:53:36 -0600

    Initial Commit

---
+++ random-photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  random-photo
+//
+//  Created by Eduardo Hernandez on 15/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
